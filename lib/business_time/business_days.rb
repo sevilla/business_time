da88877cecbf5ffa2f28e47c4dc424f7fd926c74
Tin Sevilla <kbsevilla@gmail.com>
2019-02-14 15:56:56 -0800

    Do not rely on Time.zone for time_zones; time.time_zone should be explicitly set before calling the method

--- lib/business_time/business_days.rb
+++ lib/business_time/business_days.rb
@@ -5,8 +5,9 @@ module BusinessTime
       @days = days
     end
 
-    def after(time = Time.now)
-      time = Time.zone ? Time.zone.parse(time.to_s) : Time.parse(time.to_s)
+    def after(time = Time.now.utc)
+      time = Time.parse(time) if time.is_a?(String)
+
       @days.times do
         begin
           time = time + 1.day
@@ -14,12 +15,13 @@ module BusinessTime
       end
       time
     end
-    
+
     alias_method :from_now, :after
     alias_method :since, :after
-    
-    def before(time = Time.now)
-      time = Time.zone ? Time.zone.parse(time.to_s) : Time.parse(time.to_s)
+
+    def before(time = Time.now.utc)
+      time = Time.parse(time) if time.is_a?(String)
+
       @days.times do
         begin
           time = time - 1.day
@@ -27,10 +29,10 @@ module BusinessTime
       end
       time
     end
-    
+
     alias_method :ago, :before
     alias_method :until, :before
-  
-  end  
-  
+
+  end
+
 end
